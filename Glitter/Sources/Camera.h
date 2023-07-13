5fc70db783aee51cfb3098f2f183d33df1a0cdb2
echo.zhangjie <echo.zhangjie@grabtaxi.com>
2023-07-13 23:24:20 +0800

    呆在xz平面 + 实现lookat

--- Glitter/Sources/Camera.h
+++ Glitter/Sources/Camera.h
@@ -66,7 +66,8 @@ public:
 
     // Returns the view matrix calculated using Eular Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix() {
-        return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
+//        return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
+        return calculate_lookAt_matrix(this->Position, this->Position + this->Front, this->Up);
     }
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -80,6 +81,7 @@ public:
             this->Position -= this->Right * velocity;
         if (direction == RIGHT)
             this->Position += this->Right * velocity;
+        Position.y = 0.0f; // <-- this one-liner keeps the user at the ground level (xz plane)
     }
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
@@ -126,4 +128,41 @@ private:
                                                 this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         this->Up = glm::normalize(glm::cross(this->Right, this->Front));
     }
+
+    // Custom implementation of the LookAt function
+    glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+    {
+        // 1. Position = known
+        // 2. Calculate cameraDirection
+        glm::vec3 zaxis = glm::normalize(position - target);
+        // 3. Get positive right axis vector
+        glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+        // 4. Calculate camera up vector
+        glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+        // Create translation and rotation matrix
+        // In glm we access elements as mat[col][row] due to column-major layout
+        glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+        translation[3][0] = -position.x; // Third column, first row
+        translation[3][1] = -position.y;
+        translation[3][2] = -position.z;
+        glm::mat4 rotation = glm::mat4(1.0f);
+        rotation[0][0] = xaxis.x; // First column, first row
+        rotation[1][0] = xaxis.y;
+        rotation[2][0] = xaxis.z;
+        rotation[0][1] = yaxis.x; // First column, second row
+        rotation[1][1] = yaxis.y;
+        rotation[2][1] = yaxis.z;
+        rotation[0][2] = zaxis.x; // First column, third row
+        rotation[1][2] = zaxis.y;
+        rotation[2][2] = zaxis.z;
+
+        // Return lookAt matrix as combination of translation and rotation matrix
+        return rotation * translation; // Remember to read from right to left (first translation then rotation)
+    }
+
+
+// Don't forget to replace glm::lookAt with your own version
+// view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+//    view = calculate_lookAt_matrix(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 };
